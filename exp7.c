fd265c2c8802762f98134f16b8f4866c6f8523fb
sahana <192511283.simats@saveetha.com>
2026-07-24 11:51:50 +0530

    updated exp 7

--- exp7.c
+++ exp7.c
@@ -1,15 +1,61 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int n;
+char production[10][10];
+char first[10];
+
+void findFirst(char c)
+{
+    int i, j;
+
+    if (!isupper(c))
+    {
+        printf("%c ", c);
+        return;
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        if (production[i][0] == c)
+        {
+            if (production[i][2] == '#')
+            {
+                printf("# ");
+            }
+            else if (!isupper(production[i][2]))
+            {
+                printf("%c ", production[i][2]);
+            }
+            else
+            {
+                findFirst(production[i][2]);
+            }
+        }
+    }
+}
 
 int main()
 {
-    printf("Grammar:\n");
-    printf("S -> AaAb | BbBa\n");
-    printf("A -> ε\n");
-    printf("B -> ε\n\n");
+    int i;
+    char ch;
 
-    printf("FIRST(S) = { a, b }\n");
-    printf("FIRST(A) = { ε }\n");
-    printf("FIRST(B) = { ε }\n");
+    printf("Enter number of productions: ");
+    scanf("%d", &n);
+
+    printf("Use # for epsilon\n");
+    printf("Enter productions (Example: S=Aa):\n");
+
+    for(i=0;i<n;i++)
+        scanf("%s", production[i]);
+
+    printf("\nEnter Non-Terminal to find FIRST: ");
+    scanf(" %c",&ch);
+
+    printf("\nFIRST(%c) = { ", ch);
+    findFirst(ch);
+    printf("}\n");
 
     return 0;
 }
